a846858573293e4585e1d3ce9a809df0d264e608
Mx. Kas Perch <the@nodebotani.st>
2019-12-30 20:30:24 -0600

    Finished chapter 3

--- ch3/src/main.rs
+++ ch3/src/main.rs
@@ -13,4 +13,20 @@ fn main() {
     let (y, z) = tup;
     println!("The values in y and z are {} and {}", y, z);
     println!("The values in tup are {} and {}", tup.0, tup.1);
+
+    // arrays in rust are fixed size, unlike vectors
+    let a = [1, 2, 3, 4, 5];
+    for element in a.iter() {
+        println!("This array item value is {}", element);
+    }
+    let b : [i32; 5] = [6, 7, 8, 9, 10];
+    // creates an array of 5 with all elements set to 3
+    let c = [ 3;5 ];
+
+    another_function(16);
+}
+
+// function parameters
+fn another_function(x: i32) {
+    println!("The value of the parameter is {}", x);
 }
